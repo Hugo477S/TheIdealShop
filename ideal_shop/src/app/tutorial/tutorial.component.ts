ebea3311b0b2b76ae7342c7760d01815620fd40d
hugo <hugo.rousseau.0703@protonmail.com>
2024-12-09 12:30:03 +0100

    tutorialOne

--- ideal_shop/src/app/tutorial/tutorial.component.ts
+++ ideal_shop/src/app/tutorial/tutorial.component.ts
@@ -10,22 +10,75 @@ export class TutorialComponent implements OnInit {
 
   tutorialStep: number = 0;
   tutorialSteps: Element[];
+  incOrDec!: Boolean;
 
   incrementTStep() {
     this.tutorialStep++;
+    this.incOrDec = true;
     this.launchTutorialStep();
+    
   }
   decrementTStep() {
     this.tutorialStep--;
+    this.incOrDec = false;
     this.launchTutorialStep();
+  }
+
+  regenerateTutorial(stepNumber: string){
+    for(let i=0; i< this.tutorialSteps.length-1; i++){
+
+      if(this.tutorialSteps[i].className.includes("tutorialSurlignementRed")) { // À placer au dessus de tutoSurlign à cause de contain
+        if(this.tutorialSteps[i].className.includes("filter btn-default")) {
+          this.tutorialSteps[i].className = `filter btn-default tuto tutorialStep${stepNumber}Red`;
+        } else {
+          this.tutorialSteps[i].className = `tuto tutorialStep${stepNumber}Red`;
+        }
+      }
+
+      if(this.tutorialSteps[i].className.includes("tutorialSurlignement")) {
+        if(this.tutorialSteps[i].className.includes("inpInterval")) {
+          this.tutorialSteps[i].className = `inpInterval tuto tutorialStep${stepNumber}`;
+        } else {
+          this.tutorialSteps[i].className = `tuto tutorialStep${stepNumber}`;
+        } 
+      } 
+
+      if(this.tutorialSteps[i].className.includes("tutorialBorder")){
+        this.tutorialSteps[i].className = `tuto tutorialStep${stepNumber}Border`
+      }
+
+
+        
+
+
+/*
+      if(this.tutorialSteps[i].className.includes("tutorialSurlignementRed")) {
+        if(this.tutorialSteps[i].className.includes("filter btn-default")) {
+          this.tutorialSteps[i].className = `filter btn-default tuto tutorialStep${stepNumber}Border`
+
+          this.tutorialSteps[i].className = `filter btn-default tuto tutorialStep${stepNumber}Border`;
+        }
+
+        this.tutorialSteps[i].className = `filter btn-default tuto tutorialStep${stepNumber}Border`;
+
+      }
+      */
+    }
   }
 
   launchTutorialStep(){
     console.log(this.tutorialStep)
 
-
+    if(this.tutorialStep == 0) {
+      if(!this.incOrDec) {
+        this.regenerateTutorial("One");
+      }
+    }
 
     if(this.tutorialStep == 1) {
+      if(!this.incOrDec) {
+        this.regenerateTutorial("Two");
+      }
       for(let i=0; i<this.tutorialSteps.length-1; i++) {
         if(this.tutorialSteps[i].className.includes("tutorialStepOne")) {
           this.tutorialSteps[i].className = "tutorialSurlignement";
@@ -33,24 +86,54 @@ export class TutorialComponent implements OnInit {
       }
     }
     if(this.tutorialStep == 2) {
+      // Regen Steps
+
+      if(!this.incOrDec) {
+        this.regenerateTutorial("Three");
+      } else {
+        this.regenerateTutorial("One");
+      }
+
+      
+      // Change classes
       for(let i=0; i<this.tutorialSteps.length-1; i++) {
         if(this.tutorialSteps[i].className.includes("tutorialStepTwo")) {
-          this.tutorialSteps[i].className = "tutorialSurlignement";
+          if(this.tutorialSteps[i].className.includes("inpInterval")) {
+            this.tutorialSteps[i].className = "inpInterval tutorialSurlignement";
+          } else {
+            this.tutorialSteps[i].className = "tutorialSurlignement";
+          }
         }
       }
     }
     if(this.tutorialStep == 3) {
+      if(!this.incOrDec) {
+        this.regenerateTutorial("Four");
+      } else {
+        this.regenerateTutorial("Two");
+      }
       for(let i=0; i<this.tutorialSteps.length-1; i++) {
         if(this.tutorialSteps[i].className.includes("tutorialStepThreeBorder")) {
           this.tutorialSteps[i].className = "tutorialBorder"
         } else if (this.tutorialSteps[i].className.includes("tutorialStepThreeRed")) {
-          this.tutorialSteps[i].className = "tutorialSurlignementRed";
+          if(this.tutorialSteps[i].className.includes("filter btn-default")) {
+            this.tutorialSteps[i].className = "filter btn-default tutorialSurlignementRed";
+          } else {
+            this.tutorialSteps[i].className = "tutorialSurlignementRed";
+          }
+
+          
         } else if(this.tutorialSteps[i].className.includes("tutorialStepThree")) {
           this.tutorialSteps[i].className = "tutorialSurlignement";
         }
       }
     }
     if(this.tutorialStep == 4) {
+      if(!this.incOrDec) {
+        this.regenerateTutorial("Five");
+      } else {
+        this.regenerateTutorial("Three");
+      }
       for(let i=0; i<this.tutorialSteps.length-1; i++) {
         if(this.tutorialSteps[i].className.includes("tutorialStepFourBorder")) { 
           this.tutorialSteps[i].className = "tutorialBorder"
@@ -60,11 +143,15 @@ export class TutorialComponent implements OnInit {
       }
     }
     if(this.tutorialStep == 5) {
+      this.regenerateTutorial("Four");
       for(let i=0; i<this.tutorialSteps.length-1; i++) {
-        if(this.tutorialSteps[i].className.includes("tutorialStepFiveBorder")) { 
-          this.tutorialSteps[i].className = "tutorialBorder"
+        console.log("coucou");
+        if(this.tutorialSteps[i].className.includes("tutorialStepFiveSize")) { 
+          this.tutorialSteps[i].className = "tutorialSize";
+          
         } else if (this.tutorialSteps[i].className.includes("tutorialStepFive")) {
           this.tutorialSteps[i].className = "tutorialSurlignement";
+          console.log("hahi!");
         }
       }
     }
@@ -76,7 +163,8 @@ export class TutorialComponent implements OnInit {
   }
 
   ngAfterViewInit():any {
-     this.tutorialSteps = Array.from(document.getElementsByClassName("tuto"));
+    
+    this.tutorialSteps = Array.from(document.getElementsByClassName("tuto"));
 
   }
 
